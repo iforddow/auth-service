c80ba67d56b26174a1a383e59e635624c96ed2ff
iforddow <izaakford30@gmail.com>
2025-12-07 23:09:59 -0400

    Added some code to the auth success event listener. Needs to be built on more but main concept is there.

--- auth-service/src/main/java/com/iforddow/authservice/application/listeners/AuthenticationEventListener.java
+++ auth-service/src/main/java/com/iforddow/authservice/application/listeners/AuthenticationEventListener.java
@@ -2,7 +2,12 @@ package com.iforddow.authservice.application.listeners;
 
 import com.iforddow.authservice.application.events.authentication.AuthenticationFailedEvent;
 import com.iforddow.authservice.application.events.authentication.AuthenticationSuccessfulEvent;
+import com.iforddow.authservice.auth.entity.entity.GeoLocation;
 import com.iforddow.authservice.auth.entity.jpa.Account;
+import com.iforddow.authservice.auth.entity.jpa.LoginAudit;
+import com.iforddow.authservice.auth.repository.jpa.LoginAuditRepository;
+import com.iforddow.authservice.common.service.GeoLocationService;
+import com.iforddow.authservice.common.utility.HashUtility;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,16 +15,39 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import java.time.Instant;
+
 @RequiredArgsConstructor
 @Component
 @Slf4j
 public class AuthenticationEventListener {
+
+    private final LoginAuditRepository loginAuditRepository;
+    private final HashUtility hashUtility;
+    private final GeoLocationService geoLocationService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleAuthenticationSuccessfulEvent(AuthenticationSuccessfulEvent event) {
 
         Account account = event.account();
         HttpServletRequest request = event.request();
+
+        String ipAddress = request.getRemoteAddr();
+
+        GeoLocation location =  geoLocationService.getLocation(ipAddress);
+
+        LoginAudit loginAudit = LoginAudit.builder()
+                .accountHash(hashUtility.hmacSha256(account.getId().toString()))
+                .ipAddressHash(hashUtility.hmacSha256(request.getRemoteAddr()))
+                .success(true)
+                .city(location.getCity())
+                .country(location.getCountry())
+                .countryCode(location.getCountryCode())
+                .region(location.getRegion())
+                .createdAt(Instant.now())
+                .build();
+
+        loginAuditRepository.save(loginAudit);
 
     }
 
--- auth-service/src/main/java/com/iforddow/authservice/auth/entity/jpa/LoginAudit.java
+++ auth-service/src/main/java/com/iforddow/authservice/auth/entity/jpa/LoginAudit.java
@@ -1,22 +1,24 @@
 package com.iforddow.authservice.auth.entity.jpa;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.Instant;
 import java.util.UUID;
 
-@Getter
-@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "login_audit")
 public class LoginAudit {
     @Id
+    @Builder.Default
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
-    private UUID id;
+    private UUID id = UUID.randomUUID();
 
     @Column(name = "account_hash", nullable = false)
     private String accountHash;
@@ -24,34 +26,44 @@ public class LoginAudit {
     @Column(name = "ip_address_hash", nullable = false)
     private String ipAddressHash;
 
+    @Builder.Default
     @ColumnDefault("'Unknown'")
     @Column(name = "country", nullable = false, length = 100)
-    private String country;
+    private String country = "Unknown";
 
+    @Builder.Default
     @ColumnDefault("'Unknown'")
     @Column(name = "country_code", nullable = false, length = 25)
-    private String countryCode;
+    private String countryCode = "Unknown";
 
+    @Builder.Default
     @ColumnDefault("'Unknown'")
     @Column(name = "region", nullable = false, length = 100)
-    private String region;
+    private String region = "Unknown";
 
+    @Builder.Default
     @ColumnDefault("'Unknown'")
     @Column(name = "city", nullable = false, length = 100)
-    private String city;
+    private String city = "Unknown";
 
+    @Builder.Default
+    @ColumnDefault("null")
     @Column(name = "asn", length = 50)
-    private String asn;
+    private String asn = null;
 
+    @Builder.Default
+    @ColumnDefault("null")
     @Column(name = "device_id", length = 128)
-    private String deviceId;
+    private String deviceId = null;
 
+    @Builder.Default
     @ColumnDefault("false")
     @Column(name = "success", nullable = false)
     private Boolean success = false;
 
+    @Builder.Default
     @ColumnDefault("now()")
     @Column(name = "created_at", nullable = false)
-    private Instant createdAt;
+    private Instant createdAt = Instant.now();
 
 }
